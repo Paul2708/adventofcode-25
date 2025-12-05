84896f9349a5ff4d068d86512a944a04a39c9313
Paul Hoger <paul.hoger@student.kit.edu>
2025-12-05 07:00:36 +0100

    Try solve part 2

--- Day-05/python/paul2708/day05.py
+++ Day-05/python/paul2708/day05.py
@@ -1,9 +1,11 @@
 from typing import List
 
+import tqdm
+
 from shared.paul2708.input_reader import *
 from shared.paul2708.output import write
 
-lines = read_plain_input(day=5, example=None)
+lines = read_plain_input(day=5, example=1)
 
 fresh_id_ranges = []
 ids = []
@@ -27,7 +29,73 @@ c = 0
 for i in ids:
     for a, b in fresh_id_ranges:
         if a <= i <= b:
-            c+= 1
+            c += 1
             break
 
 print(c)
+
+clean_id_ranges = []
+clean_id_ranges.append(fresh_id_ranges[0])
+
+def merge(a, b, x, y):
+    for i in range(len(clean_id_ranges)):
+        clean_a, clean_b = clean_id_ranges[i][0], clean_id_ranges[i][1]
+
+        if clean_a <= a <= b <= clean_b:
+            print("Included")
+            break
+
+        if clean_a <= a <= clean_b <= b:
+            clean_id_ranges[i] = (clean_a, b)
+            print("Right overlap")
+            break
+
+        if a <= clean_a <= b <= clean_b:
+            clean_id_ranges[i] = (a, clean_b)
+            print("Left overlap")
+            break
+
+        if a <= clean_a <= clean_b <= b:
+            clean_id_ranges[i] = (a, b)
+            print("Both overlap")
+            break
+
+        clean_id_ranges.append((a, b))
+        print("New")
+
+
+for a, b in fresh_id_ranges[1:]:
+    print(a, b)
+    for i in range(len(clean_id_ranges)):
+        clean_a, clean_b = clean_id_ranges[i][0], clean_id_ranges[i][1]
+
+        if clean_a <= a <= b <= clean_b:
+            done = True
+            print("Included")
+            break
+
+        if clean_a <= a <= clean_b <= b:
+            clean_id_ranges[i] = (clean_a, b)
+            print("Right overlap")
+            break
+
+        if a <= clean_a <= b <= clean_b:
+            clean_id_ranges[i] = (a, clean_b)
+            print("Left overlap")
+            break
+
+        if a <= clean_a <= clean_b <= b:
+            clean_id_ranges[i] = (a, b)
+            print("Both overlap")
+            break
+
+        clean_id_ranges.append((a, b))
+        print("New")
+
+total = 0
+
+for a, b in clean_id_ranges:
+    total += b - a + 1
+
+print(total)
+print(clean_id_ranges)
